11ce38588a6034a37869043d79e6f3185e5d9223
wyhong3103 <wyhong3103@gmail.com>
2023-02-17 16:28:01 +0800

    Finish skeleton of Forest comp

--- src/components/Forest.js
+++ src/components/Forest.js
@@ -1,3 +1,61 @@
 export const Forest = () => {
+    const dummy = [
+        {
+            "forestName" : "Forest 1",
+            "status" : [
+                29.1,
+                26.3,
+                93.3,
+                "Not Raining"
+            ]
+        },
+        {
+            "forestName" : "Forest 2",
+            "status" : [
+                29.9,
+                28.7,
+                89.0,
+                "Raining"
+            ]
+        },
+        {
+            "forestName" : "Forest 3",
+            "status" : [
+                28.4,
+                30.1,
+                85.2,
+                "Not Raining"
+            ]
+        },
+    ]
 
+    return(
+        <div className="forest-comp">
+            <h1>
+                Forests
+            </h1>
+            {
+                dummy.map(item => {
+                    return(
+                        <div className="forest-card">
+                            <h3>{item.forestName}</h3>
+                            <div className="forest-card-temp">
+                                <p>Temperature</p>
+                                <p>{item.status[0]}</p>
+                            </div>
+                            <div className="forest-card-humid">
+                                <p>Humidity</p>
+                                <p>{item.status[1]}</p>
+                            </div>
+                            <div className="forest-card-humid">
+                                <p>Humidity</p>
+                                <p>{item.status[2]}</p>
+                            </div>
+                            <p>Rain Status : {item.status[3]}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>    
+    )
 };
