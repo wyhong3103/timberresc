651607787eb5c78bea667551dd252411b0b5ac8f
wyhong3103 <wyhong3103@gmail.com>
2023-02-17 12:46:31 +0800

    Add dummy data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,11 @@ export const Home = () => {
     return(
         <div className="home-page">
             <Nav/>
-
+            <div className="home-page-main-container">
+                <h3>
+                    Under Construction
+                </h3>
+            </div>
         </div>
     )
 }
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -5,6 +5,11 @@ export const Team = () => {
     return(
         <div className="team-page">
             <Nav/>
+            <div className="team-page-main-container">
+                <h3>
+                    Under Construction
+                </h3>
+            </div>
         </div>
     )
 }
