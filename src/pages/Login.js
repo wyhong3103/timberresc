ec31c00dfb264f4967f64f8a4f50385a879e5233
wyhong3103 <wyhong3103@gmail.com>
2023-02-17 12:21:05 +0800

    Add navigation to '/' if logged in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AccountContext } from "../context/AcountContext";
 // Login Page
 
-export const Login = () => {
+export const Login = ({setLoggedIn, setUsername, setPfp}) => {
+    const loggedIn = useContext(AccountContext).loggedIn;
 
+    return(
+        loggedIn ?
+
+        <Navigate to="/"/>
+
+        :
+
+        <div>
+
+        </div>
+
+    )
 }
