af27e122d54aeb97447a1f4ccdab09553589936d
wyhong3103 <wyhong3103@gmail.com>
2023-02-17 17:47:41 +0800

    Finish forest style

--- src/components/Forest.js
+++ src/components/Forest.js
@@ -6,8 +6,8 @@ export const Forest = () => {
             "forestName" : "Forest 1",
             "status" : [
                 29.1,
-                26.3,
                 93.3,
+                26.3,
                 "Not Raining"
             ]
         },
@@ -15,8 +15,8 @@ export const Forest = () => {
             "forestName" : "Forest 2",
             "status" : [
                 29.9,
-                28.7,
                 89.0,
+                28.7,
                 "Raining"
             ]
         },
@@ -24,40 +24,56 @@ export const Forest = () => {
             "forestName" : "Forest 3",
             "status" : [
                 28.4,
-                30.1,
                 85.2,
+                30.1,
                 "Not Raining"
             ]
         },
     ]
 
+
     return(
         <div className="forest-comp">
-            <h1>
-                Forests
-            </h1>
-            {
-                dummy.map(item => {
-                    return(
-                        <div className="forest-card">
-                            <h3>{item.forestName}</h3>
-                            <div className="forest-card-temp">
-                                <p>Temperature</p>
-                                <p>{item.status[0]}</p>
+            <div className="forest-top">
+                <h1>
+                    Watchlist
+                </h1>
+                <button className='add-to-watchlist-btn'>
+                    Add To Watchlist
+                </button>
+            </div>
+            <div className="forest-card-container">
+                {
+                    dummy.length === 0 ? 
+                    
+                    <div className='forest-empty'>
+                        It's empty!
+                    </div>
+
+                    :
+
+                    dummy.map(item => {
+                        return(
+                            <div className="forest-card">
+                                <h3 className='forest-card-name'>{item.forestName}</h3>
+                                <div className="forest-card-temp">
+                                    <p className='forest-card-prop-title'>Temperature</p>
+                                    <p className='forest-card-prop-val'>{item.status[0]}</p>
+                                </div>
+                                <div className="forest-card-humid">
+                                    <p className='forest-card-prop-title'>Humidity</p>
+                                    <p className='forest-card-prop-val'>{item.status[1]}</p>
+                                </div>
+                                <div className="forest-card-heat">
+                                    <p className='forest-card-prop-title'>Heat Index</p>
+                                    <p className='forest-card-prop-val'>{item.status[2]}</p>
+                                </div>
+                                <p className='forest-card-rain'>Rain Status : {item.status[3]}</p>
                             </div>
-                            <div className="forest-card-humid">
-                                <p>Humidity</p>
-                                <p>{item.status[1]}</p>
-                            </div>
-                            <div className="forest-card-humid">
-                                <p>Humidity</p>
-                                <p>{item.status[2]}</p>
-                            </div>
-                            <p>Rain Status : {item.status[3]}</p>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
+            </div>
         </div>    
     )
 };
