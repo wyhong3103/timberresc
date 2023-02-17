93edc70831dbd4ce3c70d1a145e7079452d7583e
wyhong3103 <wyhong3103@gmail.com>
2023-02-17 12:13:07 +0800

    Add AccountContext Provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { AccountContext } from './context/AcountContext';
 import { Home } from './pages/Home';
 import { Forest } from './pages/Forest';
 import { Login } from './pages/Login';
+import { useState } from 'react';
 
 
 export const App = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const [pfp, setPfp] = useState("");
+
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route 
-                    path="/"
-                    element={<Home/>}
-                />
-                <Route 
-                    path="/forest"
-                    element={<Forest/>}
-                />
-                <Route 
-                    path="/login"
-                    element={<Login/>}
-                />
-            </Routes>
-        </BrowserRouter>
+        <AccountContext.Provider 
+            value = 
+            {
+                {
+                    loggedIn, username, pfp
+                }
+            }
+        >
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home/>}
+                    />
+                    <Route
+                        path="/forest"
+                        element={<Forest/>}
+                    />
+                    <Route
+                        path="/login"
+                        element=
+                        {
+                            <Login 
+                            setLoggedIn={setLoggedIn}
+                            setUsername={setUsername}
+                            setPfp={setPfp}
+                            />
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </AccountContext.Provider>
     )
 };
