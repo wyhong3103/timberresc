0e437c934b4b876455a15eccf5eb64f0924b86a8
wyhong3103 <wyhong3103@gmail.com>
2023-02-17 12:17:52 +0800

    Add navigation to /login if not logged

--- src/pages/Forest.js
+++ src/pages/Forest.js
@@ -1,8 +1,25 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AccountContext } from "../context/AcountContext";
+
 // Forest Page
 // Display average data for each forest
 // Display all Tree (Forest) in the watchlist 
 // Display the specific forest when clicked
 
 export const Forest = () => {
+    
+    const loggedIn = useContext(AccountContext).loggedIn;
 
+    return(
+        loggedIn ?
+
+        <div>
+
+        </div>
+
+        :
+
+        <Navigate to="/login"/>
+    )
 };
