72d8d2a23b40adfb7feedd761dbc41d57d833a9b
wyhong3103 <wyhong3103@gmail.com>
2023-02-18 19:25:30 +0800

    Add offset to timestamp

--- src/util/util.js
+++ src/util/util.js
@@ -1,5 +1,7 @@
 function timestampToTimeString(timestamp){
-    const date = (new Date(timestamp)).toUTCString();
+    const offset = new Date().getTimezoneOffset();
+    console.log(offset);
+    const date = (new Date(timestamp - offset*60*1000)).toUTCString();
     const re = /(.+) GMT/;
     const match = date.match(re);
     return match[1].slice(16, 25);
