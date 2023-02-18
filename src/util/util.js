a7ca294f765ed95f48bfc5a89a38746dccdd9c50
wyhong3103 <wyhong3103@gmail.com>
2023-02-18 18:20:12 +0800

    Fix conversion issue

--- src/util/util.js
+++ src/util/util.js
@@ -2,7 +2,7 @@ function timestampToTimeString(timestamp){
     const date = (new Date(timestamp)).toUTCString();
     const re = /(.+) GMT/;
     const match = date.match(re);
-    return match.slice(16, 25);
+    return match[1].slice(16, 25);
 }
 
 export{
